90aedc390b486d3228367cac376f371c90d83ae3
Aleksi Illikainen <illikainen.aleksi@gmail.com>
2018-12-18 11:25:15 +0200

    -Small changes

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(5)
+print("Waiting for a connection...")
 
 def handle_client(conn):
     """
